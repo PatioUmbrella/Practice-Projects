b2e82fd56b6dda92ed28e321298036d7816f6aa0
Ryan <43162125+PatioUmbrella@users.noreply.github.com>
2023-08-10 13:38:05 -0700

    Add files via upload

--- sql python connector practice.py
+++ sql python connector practice.py
@@ -3,10 +3,12 @@ import pandas as pd
 from sqlalchemy import create_engine, types
 
 #engine
-engine = create_engine('mysql://ryan:Abc123def!@#ghi@localhost/mydatabase') 
+engine = create_engine('mysql://root:KL45MDPI1sUx$@localhost/clients') 
 
 #csv transformation
 df = pd.read_csv("Clients.csv",sep=',',quotechar='\'',encoding='utf8') 
 
 #append table
-df.to_sql('Clients',con=engine,index=False,if_exists='append')
+df.to_sql('clients',con=engine,index=False,if_exists='append')
+
+print(df)
